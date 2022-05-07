5efd89d95658579673e5bf9811fd8c49e6d630a1
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-07 13:13:23 +0200

    null paczka, null recepta i test poprawnej dawki

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -4,11 +4,16 @@ import edu.iis.mto.testreactor.doser.infuser.Infuser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.Null;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.inOrder;
 
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
@@ -18,6 +23,9 @@ class MedicineDoserTest {
 
     private final Medicine standardMedicine = Medicine.of("Cough syrup");
     private final Capacity standardCapacity = Capacity.of(500, CapacityUnit.MILILITER);
+    private final Period standardPeriod = Period.of(12, TimeUnit.HOURS);
+    private final Dose standardDose = Dose.of(Capacity.of(20, CapacityUnit.MILILITER), standardPeriod);
+    private final Receipe standardReceipe = Receipe.of(standardMedicine, standardDose, 20);
     private final MedicinePackage standardMedicinePackage = MedicinePackage.of(standardMedicine, standardCapacity);
 
     private MedicineDoser medicineDoser;
@@ -29,9 +37,40 @@ class MedicineDoserTest {
 
     @Test
     void medicineDoserNullParameters() {
-        assertThrows(NullPointerException.class, () -> new MedicineDoser(null, dosageLog, clock));
-        assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, null, clock));
-        assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, dosageLog, null));
+        NullPointerException result = assertThrows(NullPointerException.class, () -> new MedicineDoser(null, dosageLog, clock));
+        assertEquals("infuser == null", result.getMessage());
+
+        result = assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, null, clock));
+        assertEquals("dosageLog == null", result.getMessage());
+
+        result = assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, dosageLog, null));
+        assertEquals("clock == null", result.getMessage());
+
         assertDoesNotThrow(() -> new MedicineDoser(infuser, dosageLog, clock));
+    }
+
+    @Test
+    void addNullPackage() {
+        NullPointerException result = assertThrows(NullPointerException.class, () -> medicineDoser.add(null));
+        assertEquals("medicine package == null", result.getMessage());
+
+        assertDoesNotThrow(() -> medicineDoser.add(standardMedicinePackage));
+    }
+
+    @Test
+    void nullReceipe() {
+        medicineDoser.add(standardMedicinePackage);
+        NullPointerException result = assertThrows(NullPointerException.class, () -> medicineDoser.dose(null));
+        assertEquals("medicine == null", result.getMessage());
+    }
+
+    @Test
+    void correctDose() {
+        medicineDoser.add(standardMedicinePackage);
+        assertEquals(DosingResult.SUCCESS, medicineDoser.dose(standardReceipe));
+
+        InOrder callOrder = inOrder(dosageLog);
+        callOrder.verify(dosageLog).logStart();
+        callOrder.verify(dosageLog).logEnd();
     }
 }
