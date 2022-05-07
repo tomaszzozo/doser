93442aba9caca7b5115a217ec475fdca3f79ef56
Tomasz Wojtkiewicz <222616@edu.p.lodz.pl>
2022-05-07 12:32:11 +0200

    Test czy konstruktor rzuca NullPointerException

--- src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
+++ src/test/java/edu/iis/mto/testreactor/doser/MedicineDoserTest.java
@@ -1,16 +1,37 @@
 package edu.iis.mto.testreactor.doser;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import edu.iis.mto.testreactor.doser.infuser.Infuser;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.internal.matchers.Null;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class MedicineDoserTest {
+    @Mock private Infuser infuser;
+    @Mock private DosageLog dosageLog;
+    @Mock private Clock clock;
+
+    private final Medicine standardMedicine = Medicine.of("Cough syrup");
+    private final Capacity standardCapacity = Capacity.of(500, CapacityUnit.MILILITER);
+    private final MedicinePackage standardMedicinePackage = MedicinePackage.of(standardMedicine, standardCapacity);
+
+    private MedicineDoser medicineDoser;
+
+    @BeforeEach
+    void setUp() {
+       medicineDoser = new MedicineDoser(infuser, dosageLog, clock);
+    }
 
     @Test
-    void itCompiles() {
-        assertEquals(2, 1 + 1);
+    void medicineDoserNullParameters() {
+        assertThrows(NullPointerException.class, () -> new MedicineDoser(null, dosageLog, clock));
+        assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, null, clock));
+        assertThrows(NullPointerException.class, () -> new MedicineDoser(infuser, dosageLog, null));
+        assertDoesNotThrow(() -> new MedicineDoser(infuser, dosageLog, clock));
     }
 }
